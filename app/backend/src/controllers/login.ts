17e4796dcd13c8a766438b8eab23710eeffd7dec
WalterTNFC <walter.tad@hotmail.com>
2022-10-03 16:54:05 -0300

    feat: Requisito 5

--- app/backend/src/controllers/login.ts
+++ app/backend/src/controllers/login.ts
@@ -5,6 +5,10 @@ export async function login(req: Request, res: Response) {
   const { email, password } = req.body;
   const { code, error, token } = await User.login(email, password);
 
+  if (!email) {
+    return res.status(400).json({ message: 'All fields must be filled' });
+  }
+
   if (error) {
     return res.status(code).json({ message: error });
   }
