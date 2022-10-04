fe9af863f0e7ac6b0fb30a6d4c1799da92304712
WalterTNFC <walter.tad@hotmail.com>
2022-10-04 17:53:44 -0300

    feat: loginValidate Route

--- app/backend/src/routes/login.ts
+++ app/backend/src/routes/login.ts
@@ -4,5 +4,6 @@ import * as userControllers from '../controllers/login';
 const router = Router();
 
 router.post('/', userControllers.login);
+router.get('/validate', userControllers.loginValidate);
 
 export default router;
